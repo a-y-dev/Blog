343676f0d1a6e0fcc6c1e958e84c0624bc24c014
Huawei User <gariban_bir_kul@gariban_bir_kul.com>
2022-06-25 12:42:52 +0300

    hw08

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.get("/" , (req,res) => {
-  let data = {
-    id:1,
-    title: "Blog title",
-    description: "Blog description"
-  }
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
-  res.send(data);
+app.get("/" , (req,res) => {
+  res.render("index");
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 5000;
+
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} numaralı portta başlatıldı`);
