26f6fc4c030efa5073aac723fe17b9d5c55c8dd7
Huawei User <gariban_bir_kul@gariban_bir_kul.com>
2022-06-28 12:06:47 +0300

    Update and Delete operations

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const app = express();
 const Post = require('./models/Post');
 
@@ -15,6 +16,9 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride('_method', {
+  methods: ['GET', 'POST']
+}));
 
 app.get("/" , async (req,res) => {
   const posts = await Post.find({});
@@ -29,6 +33,28 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add_post');
+});
+
+app.get('/edit/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('edit', {
+    post
+  });
+});
+
+app.put('/post-update', async (req, res) => {
+  await Post.findByIdAndUpdate(req.body.postId, {
+    title: req.body.title,
+    detail: req.body.detail
+  });
+  res.redirect('/');
+});
+
+app.delete('/delete-post/:id', async (req, res) => {
+  await Post.deleteOne({
+    _id: req.params.id
+  });
+  res.redirect('/');
 });
 
 app.get('/post', (req, res) => {
